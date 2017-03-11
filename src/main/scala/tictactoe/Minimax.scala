ac3443824a57a02ca48cb92a853b7ee0c0dd0683
chanjk <chanjk@users.noreply.github.com>
2017-03-11 23:00:32 +1100

    minimax now delays losing as long as possible, and also chooses states with higher winning chances even though a draw can be forced; added option to play the computer

--- src/main/scala/tictactoe/Minimax.scala
+++ src/main/scala/tictactoe/Minimax.scala
@@ -4,17 +4,38 @@ object Minimax {
   trait Tree[T, U] {
     def state: T
     def score: U
+    def depthFactor: Int
+    def winChance: (U, Int)
     def bestNext: Tree[T, U]
+    def search(state: T): Option[Tree[T, U]]
   }
   case class Node[T, U : Ordering](state: T, children: Seq[Tree[T, U]], player: Player) extends Tree[T, U] {
-    lazy val score = player match {
-      case Max => children.map(_.score).max
-      case Min => children.map(_.score).min
+    def search(state: T) = if (this.state == state) Some(this) else children.find(_.state == state)
+
+    lazy val (score, depthFactor) = player match {
+      case Max => children.map(x => (x.score, x.depthFactor)).max
+      case Min => children.map(x => (x.score, x.depthFactor)).min
     }
 
-    lazy val bestNext = children.find(_.score == score).get
+    lazy val winChance = children.map(_.winChance).reduce[(U, Int)] {
+      case ((s1, n1), (s2, n2)) => if (s1 == s2) (s1, n1 + n2) else player match {
+        case Max => if (implicitly[Ordering[U]].lt(s1, s2)) (s1, n1) else (s2, n2)
+        case Min => if (implicitly[Ordering[U]].gt(s1, s2)) (s1, n1) else (s2, n2)
+      }
+    }
+
+    lazy val bestNext = {
+      val candidates = scala.util.Random.shuffle(children).filter(x => (x.score, x.depthFactor) == (score, depthFactor))
+
+      if (candidates.length == 1) candidates.head
+      else player match {
+        case Max => candidates.maxBy(_.winChance)
+        case Min => candidates.minBy(_.winChance)
+      }
+    }
   }
-  case class Leaf[T, U](state: T, score: U) extends Tree[T, U] {
+  case class Leaf[T, U](state: T, score: U, depthFactor: Int, winChance: (U, Int)) extends Tree[T, U] {
+    def search(state: T) = if (this.state == state) Some(this) else None
     def bestNext = throw new NoSuchElementException
   }
 
@@ -23,10 +44,17 @@ object Minimax {
   case object Min extends Player
 
   def minimax[T, U](state: T, grow: T => Seq[T], isEndState: T => Boolean, evaluate: T => U)(implicit ord: Ordering[U]) = {
-    def iter(seed: T, currentPlayer: Player, nextPlayer: Player): Tree[T, U] =
-      if (isEndState(seed)) Leaf(seed, evaluate(seed))
-      else Node(seed, grow(seed) map (iter(_, nextPlayer, currentPlayer)), currentPlayer)
+    def iter(state: T, currentPlayer: Player, nextPlayer: Player, depth: Int): Tree[T, U] =
+      if (isEndState(state)) {
+        val score = evaluate(state)
 
-    iter(state, Max, Min)
+        currentPlayer match {
+          case Max => Leaf(state, score, depth, (score, -1))
+          case Min => Leaf(state, score, -depth, (score, 1))
+        }
+      }
+      else Node(state, grow(state) map (iter(_, nextPlayer, currentPlayer, depth + 1)), currentPlayer)
+
+    iter(state, Max, Min, 0)
   }
 }
--- src/main/scala/tictactoe/TicTacToe.scala
+++ src/main/scala/tictactoe/TicTacToe.scala
@@ -4,31 +4,53 @@ import scala.io.StdIn.readLine
 import scala.util.{ Try, Success, Failure }
 
 import Game._
+import Minimax._
+import Solver._
 
 object TicTacToe extends App {
-  def continue(board: Board, tokens: (Char, Char)): Unit = {
+  def continue(board: Board, tokens: (Char, Char), gameStates: Option[Tree[Board, Int]]): Unit = {
     val (curToken, nextToken) = tokens
 
-    Try(parse(readLine("> "))).filter { case (r, c) => isValidPos(board, r, c) } match {
-      case Success((row, col)) => {
-        val updatedBoard = board updated (indexFromPos(row, col), Some(curToken))
+    val (updatedBoard, updatedGameStates) = solverToken match {
+      case Some(t) if t == curToken => (gameStates.get.bestNext.state, Some(gameStates.get.bestNext))
+      case _ => {
+        Try(parse(readLine("> "))).filter { case (r, c) => isValidPos(board, r, c) } match {
+          case Success((row, col)) => {
+            val updatedBoard = board updated (indexFromPos(row, col), Some(curToken))
+            val updatedGameStates = gameStates match {
+              case Some(_) => gameStates.get.search(updatedBoard)
+              case _ => gameStates
+            }
 
-        println(stringify(updatedBoard))
-
-        if (isWin(updatedBoard)) println(s"'$curToken' wins!")
-        else if (isFull(updatedBoard)) println("It's a draw!")
-        else continue(updatedBoard, tokens.swap)
-      }
-      case Failure(_) => {
-        println("Invalid position. Please try again:")
-        continue(board, tokens)
+            (updatedBoard, updatedGameStates)
+          }
+          case Failure(_) => {
+            println("Invalid position. Please try again:")
+            return continue(board, tokens, gameStates)
+          }
+        }
       }
     }
+
+    println(stringify(updatedBoard))
+
+    if (isWin(updatedBoard)) println(s"'$curToken' wins!")
+    else if (isFull(updatedBoard)) println("It's a draw!")
+    else continue(updatedBoard, tokens.swap, updatedGameStates)
   }
 
   val (p1Token, p2Token) = ('O', 'X')
 
   println(stringify(emptyBoard))
-  println(s"""$p1Token to start. Enter your move as "row column" (no quotes):""")
-  continue(emptyBoard, (p1Token, p2Token))
+  println("Do you want to play against the computer (might take a while to set up)? (y/n)")
+
+  val (gameStates, solverToken) = if (readLine.toLowerCase != "y") (None, None) else {
+    println("\nDo you want to start? (y/n)")
+
+    if (readLine.toLowerCase == "y") (Some(allGameStates), Some(p2Token))
+    else (Some(allGameStates), Some(p1Token))
+  }
+
+  println(s"""\n$p1Token to start. On your turn, enter your move as "row column" (no quotes).""")
+  continue(emptyBoard, (p1Token, p2Token), gameStates)
 }
